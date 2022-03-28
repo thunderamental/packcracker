578f3698fc1b5d51fb573618e7f5c02d30018a9a
King W T <king.tkw.97@gmail.com>
2022-03-28 18:40:41 +1100

    wrote all filtration functions and processed data into txt sets.

--- pullScript.py
+++ pullScript.py
@@ -82,8 +82,9 @@ def loadSet(set) :
     getSetCardsJSON(set)
     getSetPNGs("SetData/" + set + ".txt")
 
+# loadSet("aneo")
 
-def sampler(no):
+def necSampler(no):
     with io.open("SetData/necJSON.txt", mode="r", encoding="utf-8") as f:
         parsed = json.loads(f.read())
         # print(parsed[500]) # indexes correctly. nice
@@ -98,29 +99,173 @@ def sampler(no):
         
         # json.dump! try out. 
         # can read back natively as json.loads
-sampler(0)
+# necSampler(0)
 
+# placeholder function always outputs false. terminal functor
 def falseReturner(x):
     return False
 
-def filtertime(set, condition = falseReturner):
+def trueAll(x):
+    return True
+
+# filters document for the condition provided. Outputs txt file to local directory.
+def filtertime(set, condName = "Filtered", condition = falseReturner):
     filtered = []
     with io.open("SetData/" + set + "JSON.txt", mode="r", encoding="utf-8") as f:
         parsed = json.loads(f.read())
         for card in parsed:
             if (condition(card)):
-                filtered.append([set, card["collector_number"]])
-    with open(set + "Filtered.txt", 'w') as filehandle:
-        filehandle.writelines("%s %s\n" % (card[0], card[1]) for card in filtered)
+                filtered.append([set, card["collector_number"], card["name"]])
+    with open(set + condName + ".txt", 'w') as filehandle:
+        filehandle.writelines("%s %s %s\n" % (card[0], card[1], card[2]) for card in filtered)
+    shutil.move(set + condName + '.txt', "SetRolls/" + set + condName + '.txt')
 
-def showcase(card):
+# Filtration functions
+
+def common(card):
+    if ("common" == card['rarity'] 
+        and "Saga" not in card['type_line']
+        and "Land" not in card['type_line']):
+            try:
+                if ("showcase" in card['frame_effects']) :
+                # this actually checks only if ['frame_effects'] throws a keyerror..
+                    return False
+            except:
+                print("non-showcase Common detected- single sided (not saga)")
+                return True
+    return False
+
+def uncommon(card):
+    if ("uncommon" == card['rarity'] 
+        and "Saga" not in card['type_line']):
+            try:
+                if ("showcase" in card['frame_effects']) :
+                    return False
+            except:
+                print("non-showcase Uncommon detected- single sided (not saga)")
+                return True
+    return False
+
+def sagasUC(card):
+    try:
+        if ("Saga" in card['type_line'] 
+                and "mythic" != card['rarity'] 
+                and "rare" != card['rarity']):
+            print("U/C Saga detected")
+            return True
+    except KeyError:
+        return False
+    return False
+
+def rare(card):
+    try:
+        if ("rare" == card['rarity']):
+            print("Rare detected")
+            return True
+    except KeyError:
+        return False
+    return False
+
+def mythic(card):
+    try:
+        if ("mythic" == card['rarity']):
+            print("Mythic detected")
+            return True
+    except KeyError:
+        return False
     return False
 
 def extendedart(card):
-    print("Extended art detected")
     try:
-        return ("extendedart" in card['frame_effects'])
+        if ("extendedart" in card['frame_effects']):
+            print("Extended art detected")
+            return True
     except KeyError:
         return False
 
-filtertime("nec", extendedart)
+def showcase(card):
+    try:
+        if ("showcase" in card['frame_effects']
+                and "etched" not in card['frame_effects']):
+            print("Showcase art detected - not etched")
+            return True
+    except KeyError:
+        return False
+    return False
+
+def showcaseUC(card):
+    try:
+        if ("showcase" in card['frame_effects']
+                and "rare" != card['rarity'] 
+                and "mythic" != card['rarity']):
+            print("Showcase art detected - common or uncommon")
+            return True
+    except KeyError:
+        return False
+    return False
+
+
+def ukiyo(card) :
+    try:
+        if ("fullart" in card['frame_effects']
+                and "Basic Land" in card['type_line']):
+            print("Ukiyo Land detected")
+            return True
+    except KeyError:
+        return False
+    return False
+
+def basics(card) :
+    if ("common" == card['rarity'] 
+        and "Land" in card['type_line']):
+            try:
+                if "showcase" in card['frame_effects'] :
+                    return False
+            except:
+                print("non-Ukiyo common land detected")
+                return True
+    return False
+
+def wildC(card) :
+    return ("common" == card['rarity']
+                and "Land" not in card['type_line']) # lands don't drop in WC
+
+def wildU(card) :
+    return ("uncommon" == card['rarity'])
+
+def wildR(card) :
+    return ("rare" == card['rarity'])
+
+def wildM(card) :
+    return ("mythic" == card['rarity'])
+
+print("Filters begin")
+filtertime("nec", "ExtendedArt", extendedart) # no non-crackable cards (<77)
+# filtertime("nec", "Showcase", showcase) # does not appear in packs
+filtertime("neo", "ExtendedArt", extendedart)
+filtertime("neo", "ShowcaseUC", showcaseUC)
+filtertime("neo", "SagasUC", sagasUC)
+filtertime("neo", "Common", common)
+filtertime("neo", "Uncommon", uncommon)
+filtertime("neo", "Rare", rare)
+filtertime("neo", "Mythic", mythic)
+filtertime("neo", "Ukiyo", ukiyo)
+filtertime("neo", "Basics", basics)
+
+filtertime("neo", "WCC", wildC)
+filtertime("neo", "WCU", wildU)
+
+exit()
+
+# EDITED FILES
+
+filtertime("neo", "Showcase", showcase) # removed the 4 hidetsugu.
+filtertime("neo", "WCR", wildR) # removed neo417-428 Etcheds; added necWCR to this
+filtertime("neo", "WCM", wildM) # removed neo417-428 Etcheds; added necWCR to this
+
+filtertime("nec", "WCR", wildR) # removed non-crackable cards (<77)
+filtertime("nec", "WCM", wildM) # removed non-crackable cards (<77)
+
+filtertime("neo", "Full", trueAll) # combined into neoFoil
+filtertime("aneo", "Full", trueAll) # combined into neoFoil
+filtertime("nec", "Full", trueAll) # removed non-crackable cards (<77)
